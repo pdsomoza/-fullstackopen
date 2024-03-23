6a1954e1d7de7ed0520c32a811178a6cc0ad5b60
pdsomoza <pdsomoza@gmail.com>
2024-03-22 22:55:08 -0300

    Validacion

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,75 +10,81 @@ import Notification from "./Notification";
 import personService from "./services/persons";
 
 const Type = {
-	Success: "success",
-	Error: "error"
-}
+  Success: "success",
+  Error: "error",
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [filteredPersons, setFilteredPersons] = useState([])
-  const [notificationMsg, setNotificationMsg] = useState(null)
-  const [notificationType, setNotificationType] = useState()
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState([]);
+  const [notificationMsg, setNotificationMsg] = useState(null);
+  const [notificationType, setNotificationType] = useState();
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
-      setPersons(initialPersons)
-      setFilteredPersons(initialPersons)
-    })
-  }, [])
+      setPersons(initialPersons);
+      setFilteredPersons(initialPersons);
+    });
+  }, []);
 
   const handleFilter = (e) => {
     const searchTerm = e.target.value;
     const filteredItems = persons.filter((person) =>
       person.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+    );
 
     setFilteredPersons(filteredItems);
-  }
+  };
 
   const deletePerson = (id) => {
-    const person = persons.find((p) => p.id === id)
-    if (person === undefined) return
+    const person = persons.find((p) => p.id === id);
+    if (person === undefined) return;
 
     if (window.confirm(`Delete ${person.name}?`)) {
       personService.remove(id).then(() => {
-        const updatedPersons = persons.filter(p => p.id != person.id)
-        setPersons(updatedPersons)
-        setFilteredPersons(updatedPersons)
-      })
+        const updatedPersons = persons.filter((p) => p.id != person.id);
+        setPersons(updatedPersons);
+        setFilteredPersons(updatedPersons);
+      });
     }
-  }
+  };
 
   const updatePerson = (person) => {
-    const confirm = `${person.name} is already added to phonebook, replace the old number with the new one?`
+    const confirm = `${person.name} is already added to phonebook, replace the old number with the new one?`;
     if (window.confirm(confirm)) {
-      const changedPerson = { ...person, number: newNumber }
+      const changedPerson = {
+        number: newNumber,
+        name: person.name,
+      };
+
       personService
         .update(person.id, changedPerson)
         .then((data) => {
-          const updatedPersons = persons.map(p => p.id !== person.id ? p : data)
-          setPersons(updatedPersons)
-          setFilteredPersons(updatedPersons)
+          const updatedPersons = persons.map((p) =>
+            p.id !== person.id ? p : data
+          );
+          setPersons(updatedPersons);
+          setFilteredPersons(updatedPersons);
 
-          setNotificationType(Type.Success)
-          setNotificationMsg(`Edited ${changedPerson.name}`)
-          setTimeout(() => setNotificationMsg(null), 2000)
+          setNotificationType(Type.Success);
+          setNotificationMsg(`Edited ${changedPerson.name}`);
+          setTimeout(() => setNotificationMsg(null), 2000);
         })
         .catch((error) => {
           setNotificationMsg(
             `Information of ${changedPerson.name} has already been removed from server`
-          )
-          setNotificationType(Type.Error)
-          setTimeout(() => setNotificationMsg(null), 2000)
+          );
+          setNotificationType(Type.Error);
+          setTimeout(() => setNotificationMsg(null), 2000);
 
-          const updatedPersons = persons.filter(p => p.id !== person.id)
-          setPersons(updatedPersons)
-          setFilteredPersons(updatedPersons)
-        })
+          const updatedPersons = persons.filter((p) => p.id !== person.id);
+          setPersons(updatedPersons);
+          setFilteredPersons(updatedPersons);
+        });
     }
-  }
+  };
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -87,21 +93,27 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
-      }
+      };
 
-      personService.create(personObject).then((returnedPerson) => {
-        setPersons(persons.concat(returnedPerson))
-        setFilteredPersons(persons.concat(returnedPerson))
-        
-        setNotificationType(Type.Success)
-        setNotificationMsg(`Added ${returnedPerson.name}`)
-        setTimeout(() => setNotificationMsg(null), 2000)
-      })
-    } else updatePerson(person)
+      personService
+        .create(personObject)
+        .then((newPerson) => {
+          setPersons(persons.concat(newPerson));
+          setFilteredPersons(persons.concat(newPerson));
 
-    setNewName("")
-    setNewNumber("")
-  }
+          setNotificationType(Type.Success);
+          setNotificationMsg(`Added ${newPerson.name}`);
+          setTimeout(() => setNotificationMsg(null), 2000);
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch((error) => {
+          setNotificationType(Type.Error);
+          setNotificationMsg(error.response.data.error);
+          setTimeout(() => setNotificationMsg(null), 2000);
+        });
+    } else updatePerson(person);
+  };
 
   return (
     <div>
@@ -122,7 +134,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons allPersons={filteredPersons} onDeletePerson={deletePerson} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
